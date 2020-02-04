f5277e9b31d07fd1ebdb755aba33606cce81068f
Suriya Prasath.S <48484191+SP-2605@users.noreply.github.com>
2020-02-04 22:59:04 +0530

    All required changes done

--- js/typeit.js
+++ js/typeit.js
@@ -1,18 +1,18 @@
         $(function () {
             new TypeIt('.Intro__heading-main__box-content', {
-                    speed: 100,
+                    speed: 90,
                     loop: false,
-                    waitUntilVisible: true
+                //     waitUntilVisible: true
                 })
                 .type('CSI-SIESGST')
                 .pause(500)
                 .break()
-                .type('Welcome To Innovation 2k19!')
+                .type('Welcomes You To Innovation 2k19!')
                 .pause(400)
                 .delete(3)
                 .type('20!')
-                .pause(500)
+                .pause(300)
                 .break()
                 .type('Register Your Team!')
-                .pause(500)
+                .pause(300)
         });
